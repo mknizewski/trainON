b9d3359808dc4f1d95183d78ca4852a560d9c0a4
mknizewski <mateusz.knizewski@gmail.com>
2017-01-28 11:28:23 +0100

    #mk - dodanie widoku rejestracji + zmiana ikony appki

--- TrainON/app/src/main/java/uwb/trainon/RegisterActivity.java
+++ TrainON/app/src/main/java/uwb/trainon/RegisterActivity.java
@@ -2,12 +2,20 @@ package uwb.trainon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void onClickRegisterButton(View view)
+    {
+
     }
 }
