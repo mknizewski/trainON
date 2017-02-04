872832a2233dddab0bf528443ae82006e5b85049
piotr210694 <piotr210694@wp.pl>
2017-02-04 08:07:25 +0100

    #PA - blokada obrotu

--- TrainON/app/src/main/java/uwb/trainon/MainActivity.java
+++ TrainON/app/src/main/java/uwb/trainon/MainActivity.java
@@ -1,6 +1,7 @@
 package uwb.trainon;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,7 +25,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FileManager.CreateAppFolderIfDosentExists();
-
 
         //new Thread(new waitingToStart()).start();
         //findViewById(R.id.loadingPanel).setVisibility(View.GONE); //wylaczam ladowanie
