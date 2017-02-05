4c195fa94edd23716e5c8ea6cab3f7921b41d816
piotr210694 <piotr210694@wp.pl>
2017-02-05 11:00:53 +0100

    #PA - music + animate przy uruchamianiu

--- TrainON/app/src/main/java/uwb/trainon/MainActivity.java
+++ TrainON/app/src/main/java/uwb/trainon/MainActivity.java
@@ -2,11 +2,17 @@ package uwb.trainon;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import uwb.trainon.extensions.AlertDialogExtension;
 import uwb.trainon.extensions.StringExtensions;
@@ -19,6 +25,11 @@ public class MainActivity extends AppCompatActivity
 {
     private SignInManager _signInManager;
 
+    ImageView image_logo;
+    LinearLayout panel_login;
+    Animation animation_fadein;
+    Animation animation_slideup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,10 +37,21 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         FileManager.CreateAppFolderIfDosentExists();
 
+        // animate + loading + sound
+        image_logo = (ImageView)findViewById(R.id.imageView);
+        panel_login = (LinearLayout)findViewById(R.id.panel_log);
+        animation_fadein = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        final MediaPlayer open_sound = MediaPlayer.create(this, R.raw.intro);
+
+        open_sound.start();
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
                 findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+                image_logo.setVisibility(View.VISIBLE);
+                image_logo.startAnimation(animation_fadein);
+                panel_login.setVisibility(View.VISIBLE);
+                panel_login.startAnimation(animation_fadein);
             }
         }, 3000);
     }
